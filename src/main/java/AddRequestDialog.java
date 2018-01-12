cfee5eed061df83215a644aa5558fd34fd8f53bd
xSilverline <32816233+xSilverline@users.noreply.github.com>
2018-01-12 14:42:50 +0100

    More windows! and workspejs

---
+++ src/main/java/AddRequestDialog.java
@@ -0,0 +1,48 @@
+import java.awt.event.ActionEvent;
+
+public class AddRequestDialog extends NewWindowDialog
+{
+    private Client client;
+    private MenuButton addButton;
+    private MenuButton closeButton;
+
+    AddRequestDialog(Client client)
+    {
+        this.client=client;
+        buildDialog();
+        makeGui();
+        setModalityType(ModalityType.APPLICATION_MODAL);
+        setVisible(true);
+    }
+
+    @Override
+    void makeGui()
+    {
+        addButton = new MenuButton("DODAJ");
+        closeButton = new MenuButton("ZAMKNIJ");
+
+        addButton.setBounds(300,400,200,50);
+        closeButton.setBounds(590,540,200,50);
+        add(addButton);
+        add(closeButton);
+
+        addButton.addActionListener(this);
+        closeButton.addActionListener(this);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e)
+    {
+        Object source = e.getSource();
+        if(source == closeButton)
+        {
+            dispose();
+        }
+        else if(source == addButton)
+        {
+            dispose();
+            //TODO: add to storage
+        }
+
+    }
+}
---
+++ src/main/java/EditCrewDialog.java
@@ -0,0 +1,45 @@
+import java.awt.event.ActionEvent;
+
+public class EditCrewDialog extends NewWindowDialog
+{
+    private Client client;
+    private MenuButton closeButton;
+    private MenuButton saveButton;
+
+    EditCrewDialog(Client client)
+    {
+        this.client = client;
+        buildDialog();
+        makeGui();
+        setModalityType(ModalityType.APPLICATION_MODAL);
+        setVisible(true);
+    }
+
+    @Override
+    void makeGui()
+    {
+        closeButton = new MenuButton("ZAMKNIJ");
+        closeButton.setBounds(590,540,200,50);
+        add(closeButton);
+        closeButton.addActionListener(this);
+
+        saveButton = new MenuButton("ZAPISZ");
+        saveButton.setBounds(300,500,200,50);
+        add(saveButton);
+        saveButton.addActionListener(this);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e)
+    {
+        Object source = e.getSource();
+
+        if(source == closeButton)
+        {
+            dispose();
+        } else if(source == saveButton)
+        {
+            dispose();
+        }
+    }
+}
